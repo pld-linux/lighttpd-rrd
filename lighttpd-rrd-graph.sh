c459d9a4b83b04061d9f1183b1c7f7c2cb67f396
Elan Ruusamäe <glen@pld-linux.org>
2007-01-05 12:26:42 +0000

    - do only one line as the min/max are just duplicates
    Changed files:
        lighttpd-rrd-graph.sh -> 1.3

--- lighttpd-rrd-graph.sh
+++ lighttpd-rrd-graph.sh
@@ -8,17 +8,9 @@ OUTPRE=lighttpd-traffic
 exec >/dev/null
 
 DISP="DEF:bin=$INFILE:InOctets:AVERAGE \
-      DEF:binmin=$INFILE:InOctets:MIN \
-      DEF:binmax=$INFILE:InOctets:MAX \
       DEF:bout=$INFILE:OutOctets:AVERAGE \
-      DEF:boutmin=$INFILE:OutOctets:MIN \
-      DEF:boutmax=$INFILE:OutOctets:MAX \
       LINE1:bin#0000FF:in \
-      LINE1:binmin#2222FF: \
-      STACK:binmax#2222FF: \
       LINE1:bout#FF0000:out \
-      LINE1:boutmin#FF2222: \
-      STACK:boutmax#FF2222: \
       -v bytes/s"
 
 $RRDTOOL graph $OUTDIR/$OUTPRE-hour.png -a PNG --start -14400 $DISP
@@ -28,11 +20,7 @@ $RRDTOOL graph $OUTDIR/$OUTPRE-month.png -a PNG --start -2592000 $DISP
 OUTPRE=lighttpd-requests
 
 DISP="DEF:req=$INFILE:Requests:AVERAGE \
-      DEF:reqmin=$INFILE:Requests:MIN \
-      DEF:reqmax=$INFILE:Requests:MAX \
       LINE1:req#0000FF:requests \
-      LINE1:reqmin#2222FF: \
-      STACK:reqmax#2222FF: \
       -v req/s"
 
 $RRDTOOL graph $OUTDIR/$OUTPRE-hour.png -a PNG --start -14400 $DISP
